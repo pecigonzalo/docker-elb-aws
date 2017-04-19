18f1381a612d5dc40ccdb8ab3a6927aafa1e5b8c
Gonzalo Peci <pecigonzalo@outlook.com>
2017-04-19 14:51:05 +0200

    Add new polling based controller

--- app/controller.py
+++ app/controller.py
@@ -1,7 +1,6 @@
+import time
 import logging
 import ConfigParser
-from Queue import Queue
-from threading import Thread
 
 import click
 import boto3
@@ -9,23 +8,31 @@ import docker
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger("controller")
-# Refractor to run every X because swarmKit does not have an event stream
 
 
-def getContainer(container_id, docker_client):
-    return docker_client.containers.get(container_id=container_id)
+def getServices(docker_client):
+    return docker_client.services.list()
 
 
-def getPorts(container):
-    ports = container.attrs.get("NetworkSettings").get("Ports")
-    # EG: (u'12312/tcp', [{u'HostIp': u'0.0.0.0', u'HostPort': u'1234'}])
+def getPorts(services):
+    ports = []
+    for service in services:
+        service_ports = service.attrs.get("Endpoint").get("Ports")
+        if service_ports:
+            for service_port in service_ports:
+                ports.append(
+                    service_port
+                )
+    # EG:
+    # [{u'Protocol': u'tcp',
+    # u'PublishMode': u'ingress',
+    # u'PublishedPort': 5000,
+    # u'TargetPort': 80}]
 
-    for mappings in ports.values():
-        # EG: [{u'HostPort': u'1234', u'HostIp': u'0.0.0.0'}]
-        # Check if the port is mapped
-        if mappings is not None:
-            for mapping in mappings:
-                yield mapping
+    return ports
+    # for port in ports:
+    #     # EG: {u'TargetPort': 80, u'PublishedPort': 5000, u'Protocol': u'tcp', u'PublishMode': u'ingress'}
+    #     yield port
 
 
 def createELBPort(port, elb_name, elb_client):
@@ -51,52 +58,75 @@ def destroyELBPort(port, elb_name, elb_client):
     )
 
 
-def worker(worker_id, work_queue, elb_client, elb_config, docker_client):
-    # Set debug format to log worker id
-    logger.debug("Starting worker {}".format(worker_id))
-    try:
-        while True:
-            event = work_queue.get()
+def updateELB(elb_client, elb_config, ports):
+    current_listeners = []
+    current_services = []
 
-            container_id = event.get("id")
-            container = docker_client.containers.get(container_id=container_id)
+    for port in ports:
+        current_services.append(
+            (
+                int(port.get("PublishedPort")),
+                str(port.get("Protocol").upper()),
+            )
+        )
+    current_services = sorted(current_services)
+    logger.debug('CurrentServices: {}'.format(current_services))
 
-            for mapping in getPorts(container):
-                logger.debug("Container: {} Action: {} Mapping: {}".format(
-                    container,
-                    event.get("Action"),
-                    mapping,
-                ))
-                port = mapping.get("HostPort")
-                elb_name = elb_config.get('main', mapping.get("HostIp"))
-                logger.debug("ELB: {}".format(elb_name))
+    # We dont support multipe load balancers yet so select default
+    elb_name = elb_config.get('main', "default")
 
-                if event.get("Action") == "start":
-                    logger.info("{}:AWS will create {}".format(worker_id, port))
-                    createELBPort(int(port), elb_name, elb_client)
+    listeners = elb_client.describe_load_balancers(
+            LoadBalancerNames=[elb_name]
+        ) \
+        .get("LoadBalancerDescriptions")[0] \
+        .get("ListenerDescriptions")  # Get Descriptions from array
 
-                elif event.get("Action") == "kill":
-                    logger.info("{}:AWS will destroy {}".format(worker_id, port))
-                    destroyELBPort(int(port), elb_name, elb_client)
-            work_queue.task_done()
-    except Exception as e:
-        logger.error(e)
-        pass
-        # Thread should kill main
+    for listener in listeners:
+        current_listeners.append(
+            (
+                int(listener.get("Listener").get("InstancePort")),
+                str(listener.get("Listener").get("InstanceProtocol")),
+            )
+        )
+    current_listeners = sorted(current_listeners)
+    logger.debug('CurrentListeners: {}'.format(current_listeners))
+
+    listeners_to_delete = list(set(current_listeners) - set(current_services))
+    listeners_to_create = list(set(current_services) - set(current_listeners))
+
+    if listeners_to_delete:
+        logger.info('Starting to delete unnecessary listeners on ELB: {} ({} in total).'.format(
+            elb_name, len(listeners_to_delete)))
+        logger.debug('ToDelete: {}'.format(listeners_to_delete))
+        for listener in listeners_to_delete:
+            port = listener[0]
+            destroyELBPort(port, elb_name, elb_client)
+            time.sleep(0.2)
+        logger.info('All unnecessary listeners deleted.')
+
+    if listeners_to_create:
+        logger.info('Starting to create missing listeners on ELB: {} ({} in total).'.format(
+            elb_name, len(listeners_to_create)))
+        logger.debug('ToCreate: {}'.format(listeners_to_create))
+        for listener in listeners_to_create:
+            port = listener[0]
+            createELBPort(port, elb_name, elb_client)
+            time.sleep(0.2)
+        logger.info('All missing listeners created.')
 
 
 @click.command()
 @click.option(
-    '--workers',
-    default=2,
-    envvar='WORKERS',
-    help='Number of worker threads.'
-    )
-@click.option(
     '--debug',
     default=False,
     envvar='DEBUG',
-    help='Enable or disable debug loggin.'
+    help='Enable or disable debug loggin (default False).'
+    )
+@click.option(
+    '--config',
+    default='/var/lib/docker/swarm/config.cfg',
+    envvar='ELB_CONFIG',
+    help='Enable or disable debug loggin (default False).'
     )
 @click.option(
     '--region',
@@ -104,49 +134,40 @@ def worker(worker_id, work_queue, elb_client, elb_config, docker_client):
     envvar='AWS_REGION',
     help='AWS Region to look for the ELB.'
     )
-def run(workers, debug, region):
+@click.option(
+    '--poll_interval',
+    default=3,
+    help='Polling interval in seconds (default 3).'
+    )
+def run(debug, config, region, poll_interval):
     if debug in ["True", "true", "1"]:
         logger.info("Enabling debug loggin")
         logger.setLevel(level=logging.DEBUG)
         logger.debug("Debug loggin enabled")
 
     elb_config = ConfigParser.RawConfigParser()
-    elb_config.read('/var/lib/docker/swarm/config.cfg')
+    elb_config.read(config)
 
     docker_client = docker.from_env()
     elb_client = boto3.client('elb', region_name=region)
-    work_queue = Queue()
 
-    for i in range(workers):
-        t = Thread(
-                target=worker, args=(
-                    i,
-                    work_queue,
-                    elb_client,
-                    elb_config,
-                    docker_client
-                )
-            )
-        t.daemon = True
-        t.start()
-
-    filters = {
-        'Type': 'container'
-    }
-
-    for event in docker_client.events(decode=True, filters=filters):
-        logger.debug(
-            "Action:{} Image:{}".format(event.get("Action"), event.get("id"))
+    while True:
+        node = docker_client.nodes.get(
+            docker_client.info().get("Swarm").get("NodeID")
         )
-        action = event.get("Action")
-        if action in ["start", "kill"]:
-            work_queue.put(
-                event
+        if node.attrs.get("ManagerStatus").get("Leader"):
+            ports = getPorts(
+                getServices(docker_client)
+            )
+            updateELB(
+                elb_client,
+                elb_config,
+                ports
             )
         else:
-            logger.debug("It did not match any 'start' or 'kill'")
+            logger.warn("Node is not a Leader")
 
-    work_queue.join()  # block until all tasks are done
+        time.sleep(poll_interval)
 
 
 if __name__ == '__main__':
